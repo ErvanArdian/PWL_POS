c4b3fa56b6ec73efc1eac59ce8bd7310ba31d7ef
ervanardian17 <ervanardian17@gmail.com>
2024-11-02 19:17:09 +0700

    jobsheet 10 praktikum 4

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::get('/level', [LevelController::class, 'index']);
+Route::post('/level', [LevelController::class, 'store']);
+Route::get('/level/{level}', [LevelController::class, 'show']);
+Route::put('/level/{level}', [LevelController::class, 'update']);
+Route::delete('/level/{level}', [LevelController::class, 'destroy']);
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
